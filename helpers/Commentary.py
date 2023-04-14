6bfa67066f35d3694329c7247ff5a1e152341103
Sławomir Paszko <folkus@gmail.com>
2023-04-14 11:28:03 +0200

    Fixes.

--- helpers/Commentary.py
+++ helpers/Commentary.py
@@ -1,2 +1,33 @@
-# Your OpenAI API key should be in the environment variable OPENAI_API_KEY
-OPENAI_API_KEY = 'Unknown'
+'''
+    Bible readings commentary dataclass
+
+'''
+
+from dataclasses import dataclass
+
+
+@dataclass
+class Commentary:
+    ''' Bible readings commentary dataclass'''
+    title: str = None
+    quote: str = None
+    points: list = None
+    comment: str = None
+    summary: str = None
+
+    def __post_init__(self):
+        ''' Checks if all fields are not None '''
+        if (self.title is None):
+            raise ValueError('Title is None!')
+
+        if (self.quote is None):
+            raise ValueError('Quote is None!')
+
+        if (self.points is None):
+            raise ValueError('Points is None!')
+
+        if (self.comment is None):
+            raise ValueError('Comment is None!')
+
+        if (self.summary is None):
+            raise ValueError('Summary is None!')
--- helpers/CommentaryGPT.py
+++ helpers/CommentaryGPT.py
@@ -1,7 +1,10 @@
 
+from dataclasses import asdict
+import ast
 import logging
 import os
 import openai
+from helpers.Commentary import Commentary
 
 # Get Open AI key from env
 openai.api_key = os.environ.get('OPENAI_API_KEY', None)
@@ -30,9 +33,9 @@ def get_gpt_commentary(text: str, model: str = 'gpt-3.5-turbo', temperature: flo
     # Chat GPT messages content
     messages = [
         {'role': 'system', 'content': 'Komentujesz czytania biblijne. Jesteś katolickim teologiem i biblistą.'},
-        {'role': 'user', 'content': f'Napisz komentarz do poniższego tekstu czytania w formacie : ' +
-                                    f'Tytuł komentarza, cytat świętego kościoła, 3 najwazniejsze myśli z czytania, ' +
-                                    f'akapit z komentarzem, myśl podsumowująca. Oto tekst : ${text}'},
+        {'role': 'user', 'content': f'Skomentuj tekst czytania jako json gdzie pola to (w nawiasie nazwa pola)  : ' +
+                                    f'1.(title) Tytuł komentarza, 2.(quote) cytat świętego kościoła, 3. (points) Trzy najwazniejsze myśli z czytania, ' +
+                                    f'4. (comment) akapit z komentarzem, 5. (summary) myśl podsumowująca.\n Oto czytanie: ${text}'},
     ]
 
     # Debugging
@@ -44,9 +47,29 @@ def get_gpt_commentary(text: str, model: str = 'gpt-3.5-turbo', temperature: flo
     response = openai.ChatCompletion.create(model=model,
                                             messages=messages,
                                             temperature=temperature)
-    return response
+
+    # Check : Invalid response
+    if (response is None) or ('choices' not in response) or (len(response['choices']) == 0):
+        logging.error('Open AI response is invalid!')
+        return None
+
+    # Check : Message field exists
+    if ('message' not in response['choices'][0]):
+        logging.error('Open AI response has no message!')
+        return None
+
+    # Get first choice message.
+    message = response['choices'][0]['message']
+
+    # Convert response text '{}' to python dict.
+    jsonCommentary = ast.literal_eval(message['content'])
+
+    # Convert jsonCommentary to Commentary dataclass from json.
+    commentary = Commentary(**jsonCommentary)
+
+    return commentary
 
 
 if __name__ == '__main__':
-    text = 'Tak bowiem Bóg umiłował świat, że Syna swego Jednorodzonego dał, aby każdy, kto w Niego wierzy, nie zginął, ale miał życie wieczne.(J3, 16)'
+    text = u'Tak bowiem Bóg umiłował świat, że Syna swego Jednorodzonego dał, aby każdy, kto w Niego wierzy, nie zginął, ale miał życie wieczne.(J3, 16)'
     print(get_gpt_commentary(text))
