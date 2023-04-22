c3b70097ad4776538dc485593833d0605f6468a7
Sławomir Paszko <folkus@gmail.com>
2023-04-22 23:37:17 +0200

    Fixes.

--- vectordb-create.py
+++ vectordb-create.py
@@ -16,21 +16,26 @@ from llama_index import (
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
 
+datasetpath = "trainingdata/evangeligaudium"
 
+# Datasetpath is wrong
+if (not os.path.exists(datasetpath)):
+    logging.error('Dataset path does not exist. Please check the path.')
+    sys.exit(-1)
 
 # Data set load and create if not exists
 db_index = None
-if (not os.path.exists('trainingdata/sesa.json')):
+if (not os.path.exists(f'{datasetpath}.json')):
     logging.info('Creating database...')
-    db_documents = SimpleDirectoryReader('trainingdata/sesa').load_data()
+    db_documents = SimpleDirectoryReader(datasetpath).load_data()
     db_index = GPTSimpleVectorIndex.from_documents(db_documents)
-    db_index.save_to_disk('trainingdata/sesa.json')
+    db_index.save_to_disk(f"{datasetpath}.json")
 
 if (db_index is None):
     logging.info('Loading database...')
-    db_index = GPTSimpleVectorIndex.load_from_disk('trainingdata/sesa.json')
+    db_index = GPTSimpleVectorIndex.load_from_disk(f"{datasetpath}.json")
 
-prompt = input("Podaje zapytanie do ewangelizatora:")
+prompt = input("Podaj zapytanie:")
 response = db_index.query(prompt)
 
 print("Odpowiedź: " + str(response))
